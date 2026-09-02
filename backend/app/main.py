c5e7ed6f7ffe17e11727c4f41cde67602f02a271
evfus <paslarumatei2006@gmail.com>
2026-09-02 16:43:34 +0300

    remove unused import

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.core.database import Base, engine
-from app.models import application
 from app.routers import applications, users, auth, face
 
 Base.metadata.create_all(bind = engine)
