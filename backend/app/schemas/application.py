e63bfc4ee7f4292d1b7230342ca2eb3111f43ad7
evfus <paslarumatei2006@gmail.com>
2026-09-16 16:04:29 +0300

    add non empty string validation for Application request models

--- backend/app/schemas/application.py
+++ backend/app/schemas/application.py
@@ -1,9 +1,27 @@
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
 class ApplicationCreate(BaseModel):
     name: str
     redirect_url: str
+
+    @field_validator("name", "redirect_url")
+    @classmethod
+    def if_not_empty_string(cls, v: str) -> str:
+        if not v.strip():
+            raise ValueError("Must not be empty")
+        return v
+
+class UpdateApplicationRequest(BaseModel):
+    name: str | None = None
+    redirect_url: str | None = None
+
+    @field_validator("name", "redirect_url")
+    @classmethod
+    def if_not_empty_string(cls, v: str | None) -> str | None:
+        if v is not None and not v.strip():
+            raise ValueError("Must not be empty")
+        return v
 
 class ApplicationResponse(BaseModel):
     model_config = ConfigDict(from_attributes = True)
